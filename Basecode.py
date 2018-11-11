60c467eb7d803c033fb3ec954e003fb377aff71a
dasguptaanika <32989306+dasguptaanika@users.noreply.github.com>
2018-11-11 02:49:22 -0500

    It recognizes english now! Just not punctuation yet...

--- Basecode.py
+++ Basecode.py
@@ -6,6 +6,8 @@
 #Input
 #There will be more code for this later, when we decide on input stuff
 import enchant
+import random
+
 condition = True
 while(condition == True):
     """
@@ -25,17 +27,35 @@ while(condition == True):
                 finalList.append(wordlist[x])
         if(len(finalList)>=10):
             minwords = True
-            print("Success!")
         else:
             input("Your answer must be thorough and detailed, containing\
-more than 10 unique words. ")
+more than 10 unique words. Press enter to continue. ")
             phrase = "Hello! What do you plan to achieve in your life?"
             words = input(phrase)
             wordlist = words.split();
             listlen = len(wordlist)
             finalList = []
+
+    #Figuring out if the words are english words
+    english = False
+    while(english == False):
+        d = enchant.Dict("en_US")
+        english = True
+        for(word) in (wordlist):
+            accuracy = d.check(word)
+            english =(english and accuracy)
+        if(english == False):
+            input("What you put was not in common english. Press enter to try again. ")
+            phrase = "Hello! What do you plan to achieve in your life?"
+            words = input(phrase)
+            wordlist = words.split();
+            listlen = len(wordlist)
+            finalList = [] 
+        else:
+            print("Good job! You can speak in english!")
+    print("you have terminated")    
         
     print(finalList)
     input("Press enter")
-    
+        
 
